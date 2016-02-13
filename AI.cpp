c61c51a5d8a66aa2784192a62dfb019fbad369dc
hira <mehdi.mahmoudi.s@gmail.com>
2016-02-13 16:41:52 +0330

    Simple code for test

--- AI.cpp
+++ AI.cpp
@@ -7,7 +7,6 @@
 void AI::doTurn(World *world)
 {
 	/** Fill this method. We've presented a stupid AI as an example! **/
-	srand(time(NULL));
 	std::vector<Node*>& myNodes = world->getMyNodes();
 	for(auto& source : myNodes)
 	{
@@ -16,9 +15,29 @@ void AI::doTurn(World *world)
 		if (neighbours.size() > 0)
 		{
 			/** Select a random neighbour **/
-			Node *destination = neighbours[(int)(rand() % neighbours.size())];
-			/** Move half of the nodes army to the neighbour node **/
-			world->moveArmy(source, destination, source->getArmyCount() / 2);
+			if(source->getArmyCount() > 15)
+			{
+				Node *destination = neighbours[(int)(rand() % neighbours.size())];
+				Node *destination1 = neighbours[(int)(rand() % neighbours.size())];
+				/** Move half of the nodes army to the neighbour node **/
+				if(destination->getOwner() == -1 || destination->getOwner() == world->getMyId())
+					world->moveArmy(source, destination, source->getArmyCount() / 3);
+				if(destination1->getOwner() == -1 || destination1->getOwner() == world->getMyId())
+					world->moveArmy(source, destination1, source->getArmyCount() / 3);
+			}
+			else
+			{
+				Node *destination = neighbours[(int)(rand() % neighbours.size())];
+				for(auto& ni : neighbours)
+				{
+					if(ni->getOwner() == -1)
+					{
+						destination = ni;
+						break;
+					}
+				}
+				world->moveArmy(source, destination, (source->getArmyCount() / 3) * 2);
+			}
 		}
 	}
 }
